60adcc8612a128aa2c7a91fd329bc1aee7c72e1c
vikas-srivastav <91118327+vikas-srivastav@users.noreply.github.com>
2021-10-19 18:57:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,12 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const path = require('path');
+const http = require('http');
+const port = process.env.PORT || 3000
 
-require('dotenv').config();
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-// Parsing middleware
-// Parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.urlencoded({extended: true})); // New
-
-// Parse application/json
-// app.use(bodyParser.json());
-app.use(express.json()); // New
-
-// Static Files
-app.use(express.static('public'));
-
-// Templating Engine
-app.engine('hbs', exphbs( {extname: '.hbs' }));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// Connection Pool
-// You don't need the connection here as we have it in userController
-// let connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME
-// });
- 
-
-const routes = require('./server/routes/user');
-app.use('/', routes);
-
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
